361bf5e8193c24965c3e1c619f535894dd025ea1
unknown <mathapationkar8@gmail.com>
2021-12-18 10:19:08 +0530

    Initial requirements added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Navbar} from './components';
+
+
+const App = () => {
+          return (
+               <div className='app'>
+          <div className='navbar'>
+              <Navbar />
+          </div>
+          <div className='main'>
+
+          </div>
+          <div className='footer'>
+
+          </div>
+
+               </div>
+          )
 }
 
 export default App;
