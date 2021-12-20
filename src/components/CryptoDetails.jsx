6954d5504461d0b3834445434fc1d1b372fb14fe
unknown <mathapationkar8@gmail.com>
2021-12-20 19:07:27 +0530

    cryptodetails Authontication added

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CryptoDetails = () => {
           return (
                     <div>
-                             CryptoDetails
+                             CryptoDetails Autontication
                     </div>
           )
 }
